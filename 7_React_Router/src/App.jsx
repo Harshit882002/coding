58c3ad6090c56949918012d2ed64f6ab15d8fd55
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-02-29 02:28:23 +0530

    React-Router Half Part Compleated

---
+++ 7_React_Router/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      {/*
+        <Header/>
+        <Home/>
+        <Footer/> 
+      */}
+    </>
+  )
+}
+
+export default App
---
+++ 7_React_Router/src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            React development is carried out by passionate developers
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                            
+                    React development thrives on the passion of dedicated developers who immerse themselves in crafting dynamic, efficient, and user-centric web applications.
+                     Their fervor fuels innovation, pushing the boundaries of what's possible in frontend development. 
+
+                     <p className="mt-4 text-gray-600">
+                     With a deep understanding of React's capabilities, they weave intricate code structures, architecting intuitive user experiences that captivate audiences.
+                     </p>
+
+                    <p className="mt-4 text-gray-600">Collaborative communities and endless learning opportunities drive their enthusiasm, propelling React development forward with each line of code.
+                     It's this unwavering passion that transforms ideas into reality, shaping the digital landscape and inspiring the next generation of developers.</p>
+
+
+
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
