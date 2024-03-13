c95ac545e4f6927f94cf7da1ed07ce257afadd65
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-03-14 00:17:14 +0530

    TodoContext

--- 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
+++ 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TodoProvider } from './contexts'
 
 const App = () => {
+
+  const {todos, setTodos} = useState([])
+
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
   return (
-    <div>App</div>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+       </TodoProvider>
   )
 }
 
--- 10_Context_API_with_Local_Storage_TodoList_Project/src/contexts/TodoContext.js
+++ 10_Context_API_with_Local_Storage_TodoList_Project/src/contexts/TodoContext.js
@@ -8,7 +8,11 @@ export const TodoContext = createContext({
             todo: "Todo msg",
             completed: false, 
          },
-    ]
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
 
 
