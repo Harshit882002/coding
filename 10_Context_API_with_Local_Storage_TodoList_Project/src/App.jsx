968ace5542cfd2a24a70b44508cab61b5c36e072
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-03-15 00:38:46 +0530

    No Changes

--- 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
+++ 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { TodoProvider } from './contexts'
+
 
 const App = () => {
 
@@ -15,7 +16,7 @@ const App = () => {
   }
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <TodoProvider value={ {todos, addTodo, updateTodo, deleteTodo, toggleComplete} }>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
