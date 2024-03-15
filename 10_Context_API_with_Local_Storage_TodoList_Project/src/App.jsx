2067a2773ebdeadc1ffe6ccee99f15360eebfff5
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-03-16 01:12:24 +0530

    NO Change

--- 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
+++ 10_Context_API_with_Local_Storage_TodoList_Project/src/App.jsx
@@ -16,6 +16,8 @@ const App = () => {
   }
 
   return (
+
+    
     <TodoProvider value={ {todos, addTodo, updateTodo, deleteTodo, toggleComplete} }>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
